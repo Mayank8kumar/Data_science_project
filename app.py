afd40637a6bfc546d1708d3f7d25fec31545109a
Mayank kumar <mayanksheoran8@gmail.com>
2025-06-26 17:50:39 +0530

    Data Ingestion

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 # from src.Data_science import logger
 from src.Data_science.logger import logging
 from src.Data_science.exceptions import CustomerException 
+from src.Data_science.components.data_ingestion import DataIngestion
+from src.Data_science.components.data_ingestion import DataIngestionConfig
 import sys
 
 if __name__=="__main__":
@@ -8,7 +10,10 @@ if __name__=="__main__":
 
 
     try:
-        a=1/0
+        # data_ingestion_config=DataIngestionConfig()
+        data_ingestion=DataIngestion()
+        data_ingestion.initiate_data_ingestion()
+        
     except Exception as e:
         logging.info("Custom Exception")
         raise CustomerException(e,sys)
