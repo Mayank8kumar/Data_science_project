4316cd18f9c71b67c9b10a253c33df509c067b21
Mayank kumar <mayanksheoran8@gmail.com>
2025-06-27 03:39:25 +0530

    Model training & best model pkl file

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from src.Data_science.exceptions import CustomException
 from src.Data_science.components.data_ingestion import DataIngestion
 from src.Data_science.components.data_ingestion import DataIngestionConfig
 from src.Data_science.components.data_transformation import DataTransformationConfig, DataTransformation
+from src.Data_science.components.model_trainer import ModelTrainerConfig, ModelTrainer
 import sys
 
 if __name__=="__main__":
@@ -18,7 +19,13 @@ if __name__=="__main__":
         # data_transformation_config= DataTransformationConfig()
         data_transformation= DataTransformation()
         logging.info("Data Tranformation completed ")
-        data_transformation.initiate_data_transformation(train_data_path, test_data_path)
+        train_arr, test_arr,_=data_transformation.initiate_data_transformation(train_data_path, test_data_path)
+
+
+        ## Model Training 
+
+        model_trainer= ModelTrainer()
+        print(model_trainer.initiate_model_trainer(train_arr,test_arr))
 
 
     except Exception as e:
